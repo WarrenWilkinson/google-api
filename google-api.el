ab8de3f624464fca4f3c839877ef582c7ab8df1a
Warren Wilkinson <warrenwilkinson@gmail.com>
2020-03-13 08:12:03 -0700

    Add google-api.el to provide google-api.

---
+++ google-api.el
@@ -0,0 +1,36 @@
+;;; google-api.el --- Support for the Google API -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020-2020 Warren Wilkinson
+
+;; Author: Warren Wilkinson <warrenwilkinson@gmail.com>
+;; Maintainer: Warren Wilkinson <warrenwilkinson@gmail.com>
+;; Created: 20 Feb 2020
+;; Version: 20200221
+;; Keywords: extensions
+;; Homepage: http://example.com/foo
+;; Package-Requires: (url)
+
+;; This file is not part of GNU Emacs.
+
+;; This file is part of the Emacs-Google-Api.
+
+;; Emacs-Google-Api is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; Emacs-Google-Api is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with Emacs-Google-Api.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(provide 'google-api)
+
+;;; google-api.el ends here
